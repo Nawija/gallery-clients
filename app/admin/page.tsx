d7ceea9a5ec4358bcf8db9aba0dec80d686176cf
Seovileoo <96625039+Nawija@users.noreply.github.com>
2025-10-21 10:31:56 +0200

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,10 +1,11 @@
 import UploadForm from "@/components/UploadForm";
+import Link from "next/link";
 
 export default function AdminPage() {
     return (
         <div className="container mx-auto p-6 space-y-6">
             <h1 className="text-2xl font-bold">Panel Admina</h1>
-            <UploadForm />
+            <Link href="/admin/new-client">New Client</Link>
         </div>
     );
 }
