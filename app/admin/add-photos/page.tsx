29dbf02b85d3e1590a1a242f8b8949f37c17ab08
Valkon <96625039+Nawija@users.noreply.github.com>
2025-10-21 22:16:02 +0200

    x

--- app/admin/add-photos/page.tsx
+++ app/admin/add-photos/page.tsx
@@ -1,13 +1,32 @@
 // app/admin/add-photos/page.tsx
 import Nav from "@/components/admin/Nav";
-
 import UploadForm from "@/components/admin/UploadForm";
 
-export default function UploadPhotosPage() {
+type Client = {
+    slug: string;
+};
+
+async function getClients(): Promise<Client[]> {
+    const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/admin/clients`,
+        {
+            headers: { "x-admin-pass": "seo123" },
+            cache: "no-store", // zawsze świeże dane
+        }
+    );
+
+    if (!res.ok) return [];
+    const data = await res.json();
+    return data.clients;
+}
+
+export default async function UploadPhotosPage() {
+    const clients = await getClients();
+
     return (
-        <>
+        <div className="min-h-screen">
             <Nav />
-            <UploadForm />
-        </>
+            <UploadForm clientss={clients} />
+        </div>
     );
 }
